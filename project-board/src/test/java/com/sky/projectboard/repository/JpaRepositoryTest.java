77c353164ff494d70781a9f248424475c978212c
Haneul <uhahasky@naver.com>
2022-07-12 18:04:59 +0900

    #22 - 도메인 수정 - 회원 계정과 게시글, 댓글 관계 연결
    테이블 필드 수정으로 영향 받은
    `data.sql` 테스트 데이터도 모두 업데이트
    
    하는 김에 게시글 -> 댓글 컬렉션의 정렬을 최신 순으로 적용

--- project-board/src/test/java/com/sky/projectboard/repository/JpaRepositoryTest.java
+++ project-board/src/test/java/com/sky/projectboard/repository/JpaRepositoryTest.java
@@ -2,6 +2,7 @@ package com.sky.projectboard.repository;
 
 import com.sky.projectboard.config.JpaConfig;
 import com.sky.projectboard.domain.Article;
+import com.sky.projectboard.domain.UserAccount;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -20,23 +22,45 @@ class JpaRepositoryTest {
 
     private final ArticleRepository articleRepository;
     private final ArticleCommentRepository articleCommentRepository;
+    private final UserAccountRepository userAccountRepository;
 
-    public JpaRepositoryTest(@Autowired ArticleRepository articleRepository, @Autowired ArticleCommentRepository articleCommentRepository) {
+
+    public JpaRepositoryTest(
+            @Autowired ArticleRepository articleRepository,
+            @Autowired ArticleCommentRepository articleCommentRepository,
+            @Autowired UserAccountRepository userAccountRepository) {
         this.articleRepository = articleRepository;
         this.articleCommentRepository = articleCommentRepository;
+        this.userAccountRepository = userAccountRepository;
     }
 
+    @DisplayName("select 테스트")
+    @Test
+    void givenTestData_whenSelecting_thenWorksFine() {
+        // Given
+        // When
+        List<Article> articles = articleRepository.findAll();
+        // Then
+        assertThat(articles)
+                .isNotNull()
+                .hasSize(123);
+    }
     @DisplayName("insert 테스트")
     @Test
     void givenTestData_whenInserting_thenWorksFine() {
         // Given
         long previousCount = articleRepository.count();
-        // When
-        Article savedArticle = articleRepository.save(Article.of("new article","new content", "#spring"));
-        // Then
+        UserAccount userAccount = userAccountRepository.save(UserAccount.of("uno", "pw", null, null, null));
+        Article article = Article.of(userAccount, "new article", "new content", "#spring");
 
-        assertThat(articleRepository.count()).isEqualTo(previousCount+1);
+        // When
+        articleRepository.save(article);
+
+        // Then
+        assertThat(articleRepository.count()).isEqualTo(previousCount + 1);
     }
+
+   
 
     @DisplayName("update 테스트")
     @Test
