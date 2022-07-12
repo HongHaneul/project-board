a12eea9cc247dceef9167d2bd9c70c57a6d62a99
Haneul <uhahasky@naver.com>
2022-07-12 18:42:33 +0900

    #21 - 댓글 서비스 로직의 테스트와 골격 잡기
    댓글과 관련된 단순 crud 기능을 테스트로 표현
    나머지 필요 기능은 별도 작성하여 넣기로.

---
+++ project-board/src/main/java/com/sky/projectboard/service/ArticleCommentService.java
@@ -0,0 +1,28 @@
+package com.sky.projectboard.service;
+
+import com.sky.projectboard.dto.ArticleCommentDto;
+import com.sky.projectboard.repository.ArticleCommentRepository;
+import com.sky.projectboard.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ArticleCommentService {
+
+    private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
+
+    @Transactional(readOnly = true)
+    public List<ArticleCommentDto> searchArticleComment(Long articleId) {
+        return List.of();
+    }
+
+    public void saveArticleComment(ArticleCommentDto dto) {
+    }
+
+}
